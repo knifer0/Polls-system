4846cf70c8cff45bdc98b7b68e1d72f4576a3f9b
Wojtek <Wojtek_430@interia.pl>
2021-08-08 18:53:15 +0200

    Add files via upload

---
+++ demo/src/main/java/repository/TutorialRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import java.util.List;
+
+import model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+  List<Tutorial> findByTitleContaining(String title);
+}
